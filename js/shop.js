ad047e98ab8fb77daacd264cd2a14cc5adce7e19
Anna Hilla <annahilla21@gmail.com>
2024-10-22 08:10:43 +0200

    Added cart functionality

--- js/shop.js
+++ js/shop.js
@@ -1,68 +1,10 @@
-// If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
-var products = [
-    {
-        id: 1,
-        name: 'cooking oil',
-        price: 10.5,
-        type: 'grocery',
-        offer: {
-            number: 3,
-            percent: 20
-        }
-    },
-    {
-        id: 2,
-        name: 'Pasta',
-        price: 6.25,
-        type: 'grocery'
-    },
-    {
-        id: 3,
-        name: 'Instant cupcake mixture',
-        price: 5,
-        type: 'grocery',
-        offer: {
-            number: 10,
-            percent: 30
-        }
-    },
-    {
-        id: 4,
-        name: 'All-in-one',
-        price: 260,
-        type: 'beauty'
-    },
-    {
-        id: 5,
-        name: 'Zero Make-up Kit',
-        price: 20.5,
-        type: 'beauty'
-    },
-    {
-        id: 6,
-        name: 'Lip Tints',
-        price: 12.75,
-        type: 'beauty'
-    },
-    {
-        id: 7,
-        name: 'Lawn Dress',
-        price: 15,
-        type: 'clothes'
-    },
-    {
-        id: 8,
-        name: 'Lawn-Chiffon Combo',
-        price: 19.99,
-        type: 'clothes'
-    },
-    {
-        id: 9,
-        name: 'Toddler Frock',
-        price: 9.99,
-        type: 'clothes'
-    }
-]
+let products = [];
+fetch('./products.json')
+    .then(response => response.json())
+    .then(data => {
+        products = data.products;
+    })
+    .catch(error => console.error('Error loading the JSON file:', error));
 
 // => Reminder, it's extremely important that you debug your code. 
 // ** It will save you a lot of time and frustration!
@@ -74,41 +16,95 @@ var cart = [];
 
 var total = 0;
 
+const cartList = document.getElementById("cart_list");
+const totalCart = document.getElementById("total_price");
+const countProduct = document.getElementById("count_product");
+
 // Exercise 1
 function buy(id) {
     // 1. Loop for to the array products to get the item to add to cart
-    const product = products.filter(product => product.id === id);
-    const currentProduct = product[0];
+    const product = products.find(product => product.id === id);
+
     // 2. Add found product to the cart array
-    if(cart.includes(currentProduct)) {
-        currentProduct.quantity++;
+    if (cart.includes(product)) {
+        product.quantity++;
+        countProduct.innerHTML = cart.length;
     } else {
-        currentProduct.quantity = 1;
-        cart.push(currentProduct);
+        product.quantity = 1;
+        cart.push(product);
+        countProduct.innerHTML = cart.length;
     };
-    console.log(cart);
 }
 
 // Exercise 2
 function cleanCart() {
-
+    cart = [];
+    cartList.innerHTML = '';
+    totalCart.innerHTML = calculateTotal();
+    countProduct.innerHTML = cart.length;
 }
 
 // Exercise 3
 function calculateTotal() {
     // Calculate total price of the cart using the "cartList" array
+    let totalPrice = 0;
+    cart.forEach(item => {
+        totalPrice += item.price * item.quantity;
+    })
+    return totalPrice;
 }
 
 // Exercise 4
 function applyPromotionsCart() {
     // Apply promotions to each item in the array "cart"
+    cart.forEach(item => {
+        if (item.name === "Cooking oil" && item.quantity >= 3) {
+            item.subtotalWithDiscount = item.price * 0.8;
+        } else if (item.name === "Instant cupcake mixture" && item.quantity >= 10) {
+            item.subtotalWithDiscount = item.price * 0.7;
+        } else {
+            delete item.subtotalWithDiscount;
+        }
+    })
 }
 
 // Exercise 5
 function printCart() {
     // Fill the shopping cart modal manipulating the shopping cart dom
-}
+    cartList.innerHTML = '';
 
+    applyPromotionsCart();
+
+    cart.forEach(item => {
+        const newTableRow = document.createElement("tr");
+        cartList.appendChild(newTableRow);
+
+        const newTableHeader = document.createElement("th");
+        newTableHeader.setAttribute("scope", "row");
+        newTableHeader.appendChild(document.createTextNode(`${item.name}`));
+        newTableRow.appendChild(newTableHeader);
+
+        const newTableDataForPrice = document.createElement("td");
+        const newTableDataForQuantity = document.createElement("td");
+        const newTableDataForTotal = document.createElement("td");
+
+        newTableDataForPrice.textContent = `$${item.price}`;
+        newTableDataForQuantity.textContent = item.quantity;
+
+        if (item.subtotalWithDiscount) {
+            newTableDataForTotal.textContent = `$${(item.subtotalWithDiscount * item.quantity).toFixed(2)}`;
+        } else {
+            newTableDataForTotal.textContent = `$${item.price * item.quantity}`;
+        }
+
+        newTableRow.appendChild(newTableDataForPrice);
+        newTableRow.appendChild(newTableDataForQuantity);
+        newTableRow.appendChild(newTableDataForTotal);
+
+        totalCart.innerHTML = calculateTotal();
+        console.log(calculateTotal())
+    })
+}
 
 // ** Nivell II **
 
